8b8e6a1becf74e90fcf37a977cf3d11abbb3a1d7
Edgar Mejia <edgarinhm@hotmail.com>
2025-02-25 10:00:18 -0500

    remove card comments container

--- client/src/components/task/task-card.jsx
+++ client/src/components/task/task-card.jsx
@@ -4,13 +4,11 @@ const TaskCard = ({ taskStatus, taskItem }) => {
   return (
     <div className={`${taskStatus}__items`}>
       {taskItem.title}
-      <div className="comments__container">
-        <Link to={`/comments/${taskItem.id}/${taskStatus}`}>
-          <div className="comment">
-            {taskItem.comments.length > 0 ? `View Comments` : "Add Comment"}
-          </div>
-        </Link>
-      </div>
+      <Link to={`/comments/${taskItem.id}/${taskStatus}`}>
+        <div className="comment">
+          {taskItem.comments.length > 0 ? `View Comments` : "Add Comment"}
+        </div>
+      </Link>
     </div>
   );
 };
