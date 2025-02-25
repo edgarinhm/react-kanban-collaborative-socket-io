5f40d869fbad6a3bb630d89370900375649cac85
Edgar Mejia <edgarinhm@hotmail.com>
2025-02-25 10:20:48 -0500

    add comment label validation

--- client/src/components/task/task-card.jsx
+++ client/src/components/task/task-card.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 
 const TaskCard = ({ taskStatus, taskItem }) => {
+  const commentsCount = taskItem.comments.length;
+  const commentLabel = commentsCount > 1 ? "Comments" : "Comment";
   return (
     <div className={`${taskStatus}__items`}>
       {taskItem.title}
       <Link to={`/comments/${taskItem.id}/${taskStatus}`}>
         <div className="comment">
-          {taskItem.comments.length > 0 ? `View Comments` : "Add Comment"}
+          {commentsCount > 0
+            ? `${commentsCount} ${commentLabel}`
+            : "Add Comment"}
         </div>
       </Link>
     </div>
