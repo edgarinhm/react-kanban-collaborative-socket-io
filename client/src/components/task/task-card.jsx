2ab29957b6bc91e4180aa361e95ac4ae817f8bf8
Edgar Mejia <edgarinhm@hotmail.com>
2025-02-25 09:58:34 -0500

    add comment link styles fix comments container

--- client/src/components/task/task-card.jsx
+++ client/src/components/task/task-card.jsx
@@ -4,9 +4,13 @@ const TaskCard = ({ taskStatus, taskItem }) => {
   return (
     <div className={`${taskStatus}__items`}>
       {taskItem.title}
-      <Link to={`/comments/${taskItem.id}/${taskStatus}`}>
-        {taskItem.comments.length > 0 ? `View Comments` : "Add Comment"}
-      </Link>
+      <div className="comments__container">
+        <Link to={`/comments/${taskItem.id}/${taskStatus}`}>
+          <div className="comment">
+            {taskItem.comments.length > 0 ? `View Comments` : "Add Comment"}
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
