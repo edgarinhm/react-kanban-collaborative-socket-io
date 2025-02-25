c2ad957795ad9e57f029c09fe2fcd52c85d57529
Edgar Mejia <edgarinhm@hotmail.com>
2025-02-25 17:07:52 -0500

    add api url enviroment variable

--- client/src/common/constants/environment-constants.js
+++ client/src/common/constants/environment-constants.js
@@ -1,1 +1,2 @@
 export const SOCKET_CLIENT_URL = import.meta.env.VITE_SOCKET_CLIENT_URL || "http://localhost:4000";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
--- client/src/common/services/api-routes.js
+++ client/src/common/services/api-routes.js
@@ -1,5 +1,3 @@
-export const API_URL = 'http://localhost:4000';
-
 export const loginRoute = {
     get: () => '/register',
     post: () => '/login'
