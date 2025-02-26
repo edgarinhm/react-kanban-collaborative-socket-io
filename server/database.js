7f8925295364a281b0468426b0462dd6a935b7eb
Edgar Mejia <edgarinhm@hotmail.com>
2025-02-26 09:26:38 -0500

    add mongo connection log variables

--- server/database.js
+++ server/database.js
@@ -5,6 +5,6 @@ const db = mongoose
         dbName: process.env.MONGODB_DBNAME
     })
     .then(() => console.log("Database connected!"))
-    .catch((err) => console.error(err));
+    .catch((err) => console.error('MONGODB_URI', process.env.MONGODB_URI, 'MONGODB_DBNAME', process.env.MONGODB_DBNAME, err));
 
 module.exports = db;
